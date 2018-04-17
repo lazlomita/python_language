acde7b8b2dec62b56118b4b8519d3b20564a3610
Lazlo Mita <lazlo.mita@avantica.net>
2018-04-17 10:01:04 -0400

    Complete variable types, data type conversion still needs work

--- python_variable_types_03.py
+++ python_variable_types_03.py
@@ -1,3 +1,124 @@
 #!/usr/bin/python3
 
 # Data Type Conversion
+
+# int(x [,base])
+# Converts x to an integer. The base specifies the base if x is a string.
+print (int(32.3e18))
+print (int("100"))
+print (int("100" ,8))
+print (int("64" ,8))
+print (int("100" ,16))
+print (int("64" ,16))
+
+print ("######################################")
+
+
+# float(x)
+# Converts x to a floating-point number.
+print (float("-21.9"))
+print (float("32.3e18"))
+print (float("-32.54e100"))
+print (float("70.2E12"))
+print (float("70.2E125"))
+
+print ("######################################")
+
+
+# complex(real [,imag])
+# Creates a complex number.
+
+print (complex("3.14j"))
+print (complex("45.j"))
+print (complex("9.322e-36j"))
+print (complex(".876j"))
+print (complex("-.6545+0J"))
+print (complex("3e+26J"))
+print (complex("4.53e-7j"))
+
+print ("######################################")
+
+
+# str(x)
+# Converts object x to a string representation.
+
+print (str(3.14j))
+print (str(10))
+print (str(-.6545+0J))
+print (str(True))
+
+print ("######################################")
+
+
+# repr(x)
+# Converts object x to an expression string.
+
+list = [ 'abcd', 786 , 2.23, 'john', 70.2 ]
+tuple = ( 'abcd', 786 , 2.23, 'john', 70.2  )
+print (repr(list))
+print (repr(tuple))
+
+print ("######################################")
+
+
+# eval(str)
+# Evaluates a string and returns an object.
+x = 1
+print (eval('x + 1'))
+print (eval('x'))
+
+print ("######################################")
+
+
+# tuple(s)
+# Converts s to a tuple.
+
+
+# list(s)
+# Converts s to a list.
+
+
+# set(s)
+# Converts s to a set.
+
+
+# dict(d)
+# Creates a dictionary. d must be a sequence of (key,value) tuples.
+
+
+# frozenset(s)
+# Converts s to a frozen set.
+
+
+# hex(x)
+# Converts an integer to a hexadecimal string.
+print (hex(7))
+print (hex(8))
+print (hex(9))
+print (hex(10))
+print (hex(11))
+print (hex(12))
+print (hex(13))
+print (hex(14))
+print (hex(15))
+print (hex(16))
+print (hex(17))
+
+print ("######################################")
+
+
+# oct(x)
+# Converts an integer to an octal string.
+print (oct(7))
+print (oct(8))
+print (oct(9))
+print (oct(10))
+print (oct(11))
+print (oct(12))
+print (oct(13))
+print (oct(14))
+print (oct(15))
+print (oct(16))
+print (oct(17))
+
+print ("######################################")
---
+++ python_variable_types_04.py
@@ -0,0 +1,178 @@
+#!/usr/bin/python3
+
+# chr(x)
+# Converts an integer to a character.
+# ALT + int until certain point.......
+
+# ! " # $ %
+print (chr(33) + ' ' + chr(34)  + ' ' + chr(35) + ' ' + chr(36) + ' ' + chr(37))
+# & ' ( ) *
+print (chr(38) + ' ' + chr(39)  + ' ' + chr(40) + ' ' + chr(41) + ' ' + chr(42))
+# + , - . /
+print (chr(43) + ' ' + chr(44)  + ' ' + chr(45) + ' ' + chr(46) + ' ' + chr(47))
+# 0 1 2 .... 7 8 9
+print (chr(48) + ' ' + chr(49)  + ' ' + chr(50) + ' ' + chr(55) + ' ' + chr(56) + ' ' + chr(57))
+# : ; < = > ? @
+print (chr(58) + ' ' + chr(59)  + ' ' + chr(60) + ' ' + chr(61) + ' ' + chr(62) + ' ' + chr(63) + ' ' + chr(64))
+# A B C .... X Y Z
+print (chr(65) + ' ' + chr(66)  + ' ' + chr(67) + ' ' + chr(88) + ' ' + chr(89) + ' ' + chr(90))
+# [ \ ] ^ _ `
+print (chr(91) + ' ' + chr(92)  + ' ' + chr(93) + ' ' + chr(94) + ' ' + chr(95) + ' ' + chr(96))
+# a b c .... x y z
+print (chr(97) + ' ' + chr(98)  + ' ' + chr(99) + ' ' + chr(120) + ' ' + chr(121) + ' ' + chr(122))
+# { | } ~
+print (chr(123) + ' ' + chr(124)  + ' ' + chr(125) + ' ' + chr(126))
+# Spanish
+print (chr(161)) # ¡
+print (chr(191)) # ¿
+print (chr(193)) # Á
+print (chr(201)) # É
+print (chr(205)) # Í
+print (chr(211)) # Ó
+print (chr(218)) # Ú
+print (chr(220)) # Ü
+print (chr(209)) # Ñ
+print (chr(225)) # á
+print (chr(233)) # é
+print (chr(237)) # í
+print (chr(243)) # ó
+print (chr(250)) # ú
+print (chr(252)) # ü
+print (chr(241)) # ñ
+
+# French..... TODO pending
+
+print (chr(161)) # ¡
+print (chr(162)) # ¢
+print (chr(163)) # £
+print (chr(164)) # ¤
+print (chr(165)) # ¥
+print (chr(166)) # ¦
+print (chr(167)) # §
+print (chr(168)) # ¨
+print (chr(169)) # ©
+print (chr(170)) # ª
+print (chr(171)) # «
+print (chr(172)) # ¬
+print (chr(173)) #
+print (chr(174)) # ®
+print (chr(175)) # ¯
+print (chr(176)) # °
+print (chr(177)) # ±
+print (chr(178)) # ²
+print (chr(179)) # ³
+print (chr(180)) # ´
+print (chr(181)) # µ
+print (chr(182)) # ¶
+print (chr(183)) # ·
+print (chr(184)) # ¸
+print (chr(185)) # ¹
+print (chr(186)) # º
+print (chr(187)) # »
+print (chr(188)) # ¼
+print (chr(189)) # ½
+print (chr(190)) # ¾
+print (chr(191)) # ¿
+print (chr(192)) # À
+print (chr(193)) # Á
+print (chr(194)) # Â
+print (chr(195)) # Ã
+print (chr(196)) # Ä
+print (chr(197)) # Å
+print (chr(198)) # Æ
+print (chr(199)) # Ç
+print (chr(200)) # È
+print (chr(201)) # É
+print (chr(202)) # Ê
+print (chr(203)) # Ë
+print (chr(204)) # Ì
+print (chr(205)) # Í
+print (chr(206)) # Î
+print (chr(207)) # Ï
+print (chr(208)) # Ð
+print (chr(209)) # Ñ
+print (chr(210)) # Ò
+print (chr(211)) # Ó
+print (chr(212)) # Ô
+print (chr(213)) # Õ
+print (chr(214)) # Ö
+print (chr(215)) # ×
+print (chr(216)) # Ø
+print (chr(217)) # Ù
+print (chr(218)) # Ú
+print (chr(219)) # Û
+print (chr(220)) # Ü
+print (chr(221)) # Ý
+print (chr(222)) # Þ
+print (chr(223)) # ß
+print (chr(224)) # à
+print (chr(225)) # á
+print (chr(226)) # â
+print (chr(227)) # ã
+print (chr(228)) # ä
+print (chr(229)) # å
+print (chr(230)) # æ
+print (chr(231)) # ç
+print (chr(232)) # è
+print (chr(233)) # é
+print (chr(234)) # ê
+print (chr(235)) # ë
+print (chr(236)) # ì
+print (chr(237)) # í
+print (chr(238)) # î
+print (chr(239)) # ï
+print (chr(240)) # ð
+print (chr(241)) # ñ
+print (chr(242)) # ò
+print (chr(243)) # ó
+print (chr(244)) # ô
+print (chr(245)) # õ
+print (chr(246)) # ö
+print (chr(247)) # ÷
+print (chr(248)) # ø
+print (chr(249)) # ù
+print (chr(250)) # ú
+print (chr(251)) # û
+print (chr(252)) # ü
+print (chr(253)) # ý
+print (chr(254)) # þ
+print (chr(255)) # ÿ
+print (chr(256)) # Ā
+print (chr(257)) # ā
+print (chr(258)) # Ă
+print (chr(259)) # ă
+print (chr(260)) # Ą
+print (chr(261)) # ą
+print (chr(262)) # Ć
+print (chr(263)) # ć
+print (chr(264)) # Ĉ
+print (chr(265)) # ĉ
+print (chr(266)) # Ċ
+print (chr(267)) # ċ
+print (chr(268)) # Č
+print (chr(269)) # č
+print (chr(270)) # Ď
+print (chr(271)) # ď
+print (chr(272)) # Đ
+print (chr(273)) # đ
+print (chr(274)) # Ē
+print (chr(275)) # ē
+print (chr(276)) # Ĕ
+print (chr(277)) # ĕ
+print (chr(278)) # Ė
+print (chr(279)) # ė
+print (chr(280)) # Ę
+
+print ("######################################")
+
+
+# ord(x)
+# Converts a single character to its integer value.
+
+print (ord(chr(276))) # Ĕ
+print (ord(chr(277))) # ĕ
+print (ord(chr(278))) # Ė
+print (ord(chr(279))) # ė
+print (ord(chr(280))) # Ę
+
+print ("######################################")
